e471a780154a10b9163511335cadaa9c2e643de7
Galen White <galenhwhite419@gmail.com>
2023-02-19 15:53:05 -0500

    Image carousel mostly complete; want to add slide-out transition

--- script.js
+++ script.js
@@ -11,51 +11,66 @@ const nextButton = document.getElementById('nextButton')
 // 2. Create variables to track our current image, and its two adjacent images.
 // -We will use i as our variable.
 let i = 0;
-// -currentImage - This will equal i, and will be the image we display in our carousel. It should be given a CSS class of currentImage.
-const currentImage = galleryImages[i];
-currentImage.classList.toggle('currentImage')
-// -previousImage - This will equal i-1, and will create space to the left of the image that will store the previous image in the gallery, and which can be brought forward when the user clicks the 'previous' button. It should be given a CSS class of previousImage.
+let currentImage;
 let previousImage;
-if (i === 0) {
-    previousImage = galleryImages[galleryImages.length - 1];
-}
-else {
-    previousImage = galleryImages[i - 1];
-}
-previousImage.classList.toggle('previousImage')
-// --Conditional statement - If previousImage < 0, previousImage should instead equal galleryImages.length - 1. This will create a looping effect in the gallery, taking us from the first image to the last.
-// -nextImage - This will equal i+1, and will create space to the right of the image that will store the next image in the gallery, and which can be brought forward when the user clicks the 'next' button. It should be given a CSS class of nextImage.
 let nextImage;
-if (i === galleryImages[galleryImages.length - 1]) {
-    nextImage = galleryImages[0];
+
+const imgDisplay = () => {
+    // -currentImage - This will equal i, and will be the image we display in our carousel. It should be given a CSS class of currentImage.
+    currentImage = galleryImages[i];
+    currentImage.classList.toggle('currentImage')
+    // -previousImage - This will equal i-1, and will create space to the left of the image that will store the previous image in the gallery, and which can be brought forward when the user clicks the 'previous' button. It should be given a CSS class of previousImage.
+    if (i === 0) {
+        previousImage = galleryImages[galleryImages.length - 1];
+    }
+    else {
+        previousImage = galleryImages[i - 1];
+    }
+    previousImage.classList.toggle('previousImage')
+    // --Conditional statement - If previousImage < 0, previousImage should instead equal galleryImages.length - 1. This will create a looping effect in the gallery, taking us from the first image to the last.
+    // -nextImage - This will equal i+1, and will create space to the right of the image that will store the next image in the gallery, and which can be brought forward when the user clicks the 'next' button. It should be given a CSS class of nextImage.
+    if (i === galleryImages.length - 1) {
+        nextImage = galleryImages[0];
+    }
+    else {
+        nextImage = galleryImages[i + 1];
+    }
+    nextImage.classList.toggle('nextImage')
+    // --Conditional statement - If nextImage > galleryImages.length, nextImage should instead equal 0. This will create a looping effect in the gallery, taking us from the last image to the first.
 }
-else {
-    nextImage = galleryImages[i + 1];
-}
-nextImage.classList.toggle('nextImage')
-// --Conditional statement - If nextImage > galleryImages.length, nextImage should instead equal 0. This will create a looping effect in the gallery, taking us from the last image to the first.
+imgDisplay();
 
 // 3. Create two onclick event listeners respective to the previous and next buttons.
 // -previousButton
 // --onClick - i--. By decrementing the value of i, the previous image in the gallery should become the current image and thus be displayed.
 previousButton.addEventListener('click', () => {
-    console.log('Previous image');
     currentImage.classList.toggle('currentImage');
     previousImage.classList.toggle('previousImage');
     nextImage.classList.toggle('nextImage');
     if (i === 0) {
         i = galleryImages.length - 1;
+        imgDisplay();
     }
     else {
         i--;
+        imgDisplay();
     }
-    currentImage.classList.toggle('currentImage');
-    previousImage.classList.toggle('previousImage');
-    nextImage.classList.toggle('nextImage');
+    // currentImage.classList.add('currentImage');
+    // previousImage.classList.add('previousImage');
+    // nextImage.classList.add('nextImage');
 })
 // -nextButton
 // --onClick - i++. By incrementing the value of i, the next image in the gallery should become the current image and thus be displayed.
 nextButton.addEventListener('click', () => {
-    console.log('Next image');
-    i++;
+    currentImage.classList.toggle('currentImage');
+    previousImage.classList.toggle('previousImage');
+    nextImage.classList.toggle('nextImage');
+    if (i === galleryImages.length - 1) {
+        i = 0;
+        imgDisplay();
+    }
+    else {
+        i++;
+        imgDisplay();
+    }
 })

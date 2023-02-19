557747d9356adb26f3ec47367a9aaf366df07591
Galen White <galenhwhite419@gmail.com>
2023-02-18 19:59:09 -0500

    Setting up for image carousel

---
+++ script.js
@@ -0,0 +1,7 @@
+// Carousel
+
+// I need to:
+// -Select image containers
+// -Select carousel buttons
+// -Cause the right button to navigate to the next item in the gallery
+// -Cause the left button to navigate to the previous item in the gallery
